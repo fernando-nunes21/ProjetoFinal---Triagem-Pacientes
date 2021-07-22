2ae6fea6b0f75d4bc63df0fab77b2aca808f40c6
fernando-nunes21 <nmgamesforfun@gmail.com>
2021-07-21 22:43:19 -0300

    Sistema de cadastro de Paciente concluido.

--- src/main/java/ProjetoFinal/TriagemPacientes/Main.java
+++ src/main/java/ProjetoFinal/TriagemPacientes/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        BancoAplicacao bancoAplicacao = new BancoAplicacao();
         Integer menuInput;
         do {
         exibirMenu();
